d7839e867d8c7770129cfde0a24f9388cbf3e2fe
Sangram <sangram15.behera@gmail.com>
2022-05-31 03:01:13 +0530

    Leap year

---
+++ LeapYear/LeapYear/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        int year;
+        Console.Write("\n\n");
+        Console.Write("Check whether a given year is leap year or not:\n");
+        Console.Write("Input an year : ");
+        year = Convert.ToInt32(Console.ReadLine());
+
+        if ((year % 4) == 0)
+            Console.WriteLine(" is a leap year.", year);
+        else
+            Console.WriteLine(" is not a leap year.", year);
+    }
+}
